9a5de0577df6cdb253de8721fbd2d828763e14cd
Matias Garcia <matiasigarcia91@gmail.com>
2021-10-05 17:08:33 +0200

    Add some REST routes

--- REST.md
+++ REST.md
@@ -1,1 +1,51 @@
-### REST
+### REST - REpresentational State Transfer
+
+1. Operations as HTTP methods
+2. Respond with appropiate status codes
+3. Clean urls.
+
+Endpoint => Method + url
+
+## 1. Operations as HTTP methods
+
+HTTP Methods:
+GET PUT DELETE POST PATCH
+
+CRUD => Create Read Delete Update  
+ POST GET DELETE PUT & PATCH
+
+Read users data:
+app.get('/users');
+app.post('/users');
+
+# 2. Respond with appropriate status codes:
+
+200.. Success Ok
+300.. Redirect Not here
+400.. User/client error you screwed up
+500.. Server errors I screwed up
+
+200 - OK
+
+400 - Bad request
+401 - Unauthorized
+404 - Not found
+
+500 - Server Error
+
+# 3. Clean urls
+
+1.  GET - '/fetchUsers'
+    POST - '/createUser'
+    GET - '/userWithLists'
+    GET - '/allLists'
+
+2.  GET - '/users'
+    POST - '/users'
+    GET = '/users/:id'
+    PATCH - '/users/:id'
+
+RPC - Remote Procedure Call
+POST - '/sendEmail'
+
+GET - '/lists'
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const TodoItem = require("./models").todoItem;
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/test", async (req, res, next) => {
   res.send("Hellooooo");
 });
@@ -26,6 +28,40 @@ app.get("/lists", async (req, res, next) => {
       include: [{ model: TodoItem }],
     });
     res.send(lists);
+  } catch (e) {
+    next(e); // pass error to express.
+  }
+});
+
+app.get("/users/:id", async (req, res, next) => {
+  try {
+    // define the id => get it from => req.params
+    const userId = req.params.id;
+    const user = await User.findByPk(userId);
+
+    if (!user) {
+      res.status(404).send("Hey we didn't find your user");
+    } else {
+      res.send(user);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+app.post("/users", async (req, res, next) => {
+  try {
+    // name, password, email
+
+    console.log(req.body); // body of the request will bring my data/
+    const { email, name, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).send("Missing parameters to create a user");
+    } else {
+      const newUser = await User.create({ name, email, password });
+      res.send(newUser);
+    }
   } catch (e) {
     next(e);
   }
